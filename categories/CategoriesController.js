5fbf4ce426d40f5e6e59266125245e1e2ab594ef
Breno de Barros <brenowbarros21@gmail.com>
2022-02-12 19:55:08 -0400

    Criando formulario para publicacao de artigos

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -3,10 +3,40 @@ const router = express.Router();
 const Category = require("./Category");
 const slugify = require("slugify");
 
+// ========== GET Routes ==========
+
+//List categories
+router.get('/admin/categories', (req, res) => {
+    Category.findAll().then(categories => {
+        res.render('admin/categories/index', {categories: categories});
+    });
+});
+
+//Form new category
 router.get('/admin/categories/new', (req, res) => {
     res.render('admin/categories/new');
 });
 
+//Form edit category
+router.get('/admin/categories/edit/:id', (req, res) => {
+    var id = req.params.id;
+    if (isNaN(id)) {
+        res.redirect('/admin/categories');
+    }
+    Category.findByPk(id).then(category => {
+        if(category != undefined) {
+            res.render('admin/categories/edit', {category: category});
+        } else {
+            res.redirect('/admin/categories');
+        }
+    }).catch(error => {
+        res.redirect('/admin/categories');
+    });
+});
+
+// ========== POST Routes ==========
+
+//Save new category
 router.post('/categories/save', (req, res) => {
     var title = req.body.title;
     if(title != undefined) {
@@ -21,12 +51,7 @@ router.post('/categories/save', (req, res) => {
     }
 });
 
-router.get('/admin/categories', (req, res) => {
-    Category.findAll().then(categories => {
-        res.render('admin/categories/index', {categories: categories});
-    });
-});
-
+//Delete category
 router.post('/categories/delete', (req, res) => {
     var id = req.body.id;
     if(id != undefined) {
@@ -46,22 +71,7 @@ router.post('/categories/delete', (req, res) => {
     }
 });
 
-router.get('/admin/categories/edit/:id', (req, res) => {
-    var id = req.params.id;
-    if (isNaN(id)) {
-        res.redirect('/admin/categories');
-    }
-    Category.findByPk(id).then(category => {
-        if(category != undefined) {
-            res.render('admin/categories/edit', {category: category});
-        } else {
-            res.redirect('/admin/categories');
-        }
-    }).catch(error => {
-        res.redirect('/admin/categories');
-    });
-});
-
+//Save edited category
 router.post('/categories/update', (req, res) => {
     var id = req.body.id;
     var title = req.body.title;
