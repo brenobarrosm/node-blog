89a4603e3c2998c72833a8d2b0ca3f84be7a9a84
Breno de Barros <brenowbarros21@gmail.com>
2022-02-15 20:39:53 -0400

    Implementando funcionalide de editar artigo

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -4,6 +4,8 @@ const Category = require("../categories/Category");
 const Article = require("../articles/Article");
 const slugify = require("slugify");
 
+// ========== GET ROUTES ==========
+
 //List articles
 router.get('/admin/articles', (req, res) => {
     Article.findAll({
@@ -20,6 +22,25 @@ router.get('/admin/articles/new', (req, res) => {
     });
     
 });
+
+//Form edit article
+router.get('/admin/articles/edit/:id', (req, res) => {
+    var id = req.params.id;
+    Article.findByPk(id).then(article => {
+        if(article != undefined) {
+            Category.findAll().then(categories => {
+                res.render('admin/articles/edit', {article: article, categories: categories});
+            });
+            
+        } else {
+            res.redirect('/admin/articles');
+        }
+    }).catch(err => {
+        res.redirect('/admin/articles');
+    });
+});
+
+// ========== POST ROUTES ==========
 
 //Save article
 router.post('/articles/save', (req, res) => {
@@ -58,20 +79,26 @@ router.post('/articles/delete', (req, res) => {
     }
 });
 
-//Form edit article
-router.get('/admin/articles/edit/:id', (req, res) => {
-    var id = req.params.id;
-    Article.findByPk(id).then(article => {
-        if(article != undefined) {
-            Category.findAll().then(categories => {
-                res.render('admin/articles/edit', {article: article, categories: categories});
-            });
-            
-        } else {
-            res.redirect('/admin/articles');
+//Update article
+router.post('/articles/update', (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.update({
+        title: title,
+        body: body,
+        categoryId: category,
+        slug: slugify(title)
+    }, {
+        where: {
+            id: id
         }
+    }).then(() => {
+        res.redirect('/admin/articles')
     }).catch(err => {
-        res.redirect('/admin/articles');
+        res.redirect('/admin/articles')
     });
 });
 
