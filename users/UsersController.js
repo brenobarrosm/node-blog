f3dab047e5c8151caca54de5f74f36048867e366
Breno de Barros <brenowbarros21@gmail.com>
2022-02-18 11:32:35 -0400

    Salvando usuario no banco

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,13 +1,50 @@
 const express = require("express");
 const router = express.Router();
 const User = require("./User");
+const bcrypt = require("bcryptjs");
 
+// ========== GET ROUTES ==========
+
+//List users
 router.get('/admin/users', (req, res) => {
     res.send('Listagem de usuarios');
 });
 
+//Form create user
 router.get('/admin/users/create', (req, res) => {
     res.render('admin/users/create');
+});
+
+// ========== POST ROUTES ==========
+
+//Save user
+router.post('/users/save', (req, res) => {
+    var email = req.body.email;
+    var password = req.body.password;
+
+    User.findOne({
+        where: {
+            email: email
+        }
+    }).then(user => {
+        if(user == undefined) {
+            var salt = bcrypt.genSaltSync(10);
+            var hash = bcrypt.hashSync(password, salt);
+        
+            User.create({
+                email: email,
+                password: hash
+            }).then(() => {
+                res.redirect('/');
+            }).catch(err => {
+                res.redirect('/');
+            });
+        } else {
+            res.redirect('/admin/users/create')
+        }
+    });
+
+    
 });
 
 module.exports = router;
